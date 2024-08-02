95816c51d05e40269b317f9169e081429b6ed9ab
MinorSc <misolano1192@gmail.com>
2024-08-01 20:39:04 -0600

    Feat Entregable6

--- js/Main.js
+++ js/Main.js
@@ -4,4 +4,18 @@ import { MostrarHtml } from "/js/CrearHML.js"
 //Componentes
 document.addEventListener("DOMContentLoaded", ev => {
     MostrarHtml()
+    MostrarDatosConfiguracion()
 })
+
+function MostrarDatosConfiguracion() {
+    //Truco !Fetch
+let url ;
+
+    fetch(url)
+    
+        .then(response => response.json())
+    
+        .then(data => console.log(data));
+    
+
+}
