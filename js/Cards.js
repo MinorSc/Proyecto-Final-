51da1957624c468948da7c00da8caeb144abd258
MinorSc <misolano1192@gmail.com>
2024-08-06 22:41:46 -0600

    Tarea JS 6

--- js/Cards.js
+++ js/Cards.js
@@ -1,36 +1,45 @@
-import {ObtenerDatosApiRick } from "/js/Servicios_Config.js"
+import {ObtenerDatosApiRick} from "/js/Servicios_Config.js"
 
-export const MostrarCard = async() => {
+
+export const  MostrarCard= async() => {
 
     const Datos = await ObtenerDatosApiRick()
 
     if (Datos) {
-
-          let Noticias = document.getElementById("Noticias").innerHTML = CrearTarjetaNoticias(Datos);
+        
+        let Noticias = document.getElementById("Noticias").innerHTML= CrearTarjetaNoticias(Datos);
         
     }
 
+   
 
-const CrearTarjetaNoticias=(Datos) => {
-    let html = ""
+    
+}
+
+const CrearTarjetaNoticias=(Datos)=>  {
+
+    let html= ""
+
+
     Datos.forEach(element => {
         html += `
 
-        <section class=" banner  col-lg-4 ">
-                        <div class="card h-100">
-                            <img src="${element.image}" class="card-img-top" alt="...">
-                            <div class="card-body">
-                                <h5 class="card-title">${element.name}</h5>
-                                <p class="card-text">${element.id}</p>
-                                <a href="#" class="btn btn-primary">Mas información <i
-                                        class="fas fa-angle-double-right"></i></a>
-                            </div>
-                        </div>
-                    </section>
-        `
+     <section class=" banner  col-lg-4 ">
+          <div class="card h-100">
+            <img src="${element.image}" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">${element.name}</h5>
+              <p class="card-text">${element.id}</p>
+              <a href="#" class="btn btn-primary">Más información <i class="fas fa-angle-double-right"></i></a>
+            </div>
+          </div>
+        </section>
+
+    `
     });
-    
+
+     
 
     return html
-} 
 }
+
